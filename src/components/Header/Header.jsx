599f14e7ccc613822bd2244c483e6f32e3f68816
DariusBlac <legionnsspace@gmail.com>
2023-10-08 21:10:22 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,14 @@ export const Header = () => {
   return (
     <>
       <header>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/movies">Movies</NavLink>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies">Movies</NavLink>
+          </li>
+        </ul>
       </header>
 
       <Suspense>
